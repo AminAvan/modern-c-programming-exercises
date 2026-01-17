3684da38f60deb139dd9a1b5f5559a3715edd6a2
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-17 12:44:39 -0500

    start reviewing

--- example/example4_1/example4_1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    int result = 0;
-    result  = (1 + 2) * 4;
-    printf("result is %d", result);    
-    return(0);
-}
--- exercise2/exercise2_1/exercise2_1.c
+++
@@ -1,7 +0,0 @@
-// On your computer, type in the hello program and execute it
-#include <stdio.h>
-
-int main () {
-    printf("hello world!");
-    return(0);
-}
