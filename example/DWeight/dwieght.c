427f377dd950d0b7ca941c92408f261a137865a3
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-18 17:13:05 -0500

    New example: dimensional weight

---
+++ example/DWeight/dwieght.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int length, width, height, volume, dim_weight, const_vol_weight, round_up_weight;
+    length = 12;
+    width = 10;
+    height = 8;
+    const_vol_weight = 166;
+    round_up_weight = 165;
+
+    volume = length * width * height;
+    dim_weight = (volume + round_up_weight) / const_vol_weight;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", dim_weight);
+
+    return 0;
+}
