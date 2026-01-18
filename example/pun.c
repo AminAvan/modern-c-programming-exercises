d7bc8916a1cc25f34f76cb09c7b688aa5f0ce785
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-18 16:12:11 -0500

    comments has been added to pun.c

--- example/pun.c
+++ example/pun.c
@@ -1,5 +1,10 @@
+/* Name: pun.c */
+/* Purpose: Prints a bad pun. */
+/* Author: K. N. King */
+
 #include <stdio.h>
 
+// Bad pun starts from the main.
 int main (void){
     printf("To C, or not to C: that is the question.\n -- Shakespeare\n");
     return 0;
