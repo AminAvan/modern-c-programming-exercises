2574d4702098cab022b460f20580f4699fed8173
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-18 22:00:50 -0500

    Update cnvt_fahr_to_cels.c with MACRO DEFINITION.

--- example/convert_fahr_to_cels/cnvt_fahr_to_cels.c
+++ example/convert_fahr_to_cels/cnvt_fahr_to_cels.c
@@ -4,13 +4,16 @@ Converts a fahrenheit temperature to celsius.
 
 #include <stdio.h>
 
+#define FREEZING_PT 32.0f
+#define SCALE_RATIO (5.0f/9.0f)
+
 int main(void){
     float tmp_fahr, tmp_c;
 
     printf("Enter Fahrenheit temprature: ");
     scanf("%f", &tmp_fahr);
     
-    tmp_c = (tmp_fahr - 32.0) * (5.0/9.0);
+    tmp_c = (tmp_fahr - FREEZING_PT) * (SCALE_RATIO);
 
     printf("\nEquivalent temprature in Celsius: %.1f\n", tmp_c);
 
