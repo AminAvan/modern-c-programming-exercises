895691da51c37cefb31993a2dfc22ad2e086fa84
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-23 02:56:40 -0500

    Add: exercise\Chapter3\isbn_breakdown\isbn_breakdown.c

---
+++ exercise/Chapter3/isbn_breakdown/isbn_breakdown.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void){
+    int gsi_prefix, group_id, publisher_code, item_num, check_digit;
+    
+    printf("Enter ISBN: ");
+    scanf("%d-%d-%d-%d-%d", &gsi_prefix, &group_id, &publisher_code, &item_num, &check_digit);
+
+    printf("GSI prefix: %d\n", gsi_prefix);
+    printf("Group identifier: %d\n", group_id);
+    printf("Publisher code: %d\n", publisher_code);
+    printf("Item number: %d\n", item_num);
+    printf("Check digit: %d\n", check_digit);
+    
+    return 0;
+}
