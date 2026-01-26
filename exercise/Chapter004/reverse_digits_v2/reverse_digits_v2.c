48daf1e716786d9d5a08ac7e22228fe5a75fd717
Amin Avan <aminavan.hardware@yahoo.com>
2026-01-26 01:02:38 -0500

    ADD: exercise\Chapter004\reverse_digits_v2\reverse_digits_v2.c

---
+++ exercise/Chapter004/reverse_digits_v2/reverse_digits_v2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int input, output;
+
+    printf("Enter a two-digit number: ");
+    scanf("%d", &input);
+
+    output = ((input % 10) * 10) + (input / 10);
+    printf("The reversal is: %02d", output);
+
+    return 0;
+}
