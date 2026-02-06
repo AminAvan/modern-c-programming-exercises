32713006ffd5b44c80a6ad52616fcfa2184f6ba6
Amin Avan <aminavan.hardware@yahoo.com>
2026-02-06 13:41:07 -0500

    debug

---
+++ exercise/Chapter005/find_largest_smallest/find_largest_smallest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int inp_1, inp_2, inp_3, inp_4;
+    
+    printf("Enter four integers: ");
+    scanf("%d %d %d %d", &inp_1, &inp_2, &inp_3, &inp_4);
+
+    printf("%d, %d, %d, %d.", inp_1, inp_2, inp_3, inp_4);
+
+    return 0;
+}
+
+//D:\Amin Files from OT laptop\Learn C\C_review\exercise\Chapter005\find_largest_smallest\find_largest_smallest.c
