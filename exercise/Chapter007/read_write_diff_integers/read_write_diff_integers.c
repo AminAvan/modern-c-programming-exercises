3e798ad6d9e30fa2fbc8b68eb3c698020bde7d90
Amin Avan <aminavan.hardware@yahoo.com>
2026-02-19 01:29:07 -0500

    UPDATE: exercise\Chapter007\read_write_diff_integers\read_write_diff_integers.c

--- exercise/Chapter007/read_write_diff_integers/read_write_diff_integers.c
+++ exercise/Chapter007/read_write_diff_integers/read_write_diff_integers.c
@@ -2,6 +2,7 @@
 
 int main(void){
     unsigned int a, b, c;
+    short int dd, ee, ff;
 
     printf("\nwrite a number in base-10: ");
     scanf("%u", &a);
@@ -14,6 +15,19 @@ int main(void){
     printf("\nwrite a number in base-16: ");
     scanf("%x", &c);
     printf("The entered number in base-16 is: %x \n", c);
+
+
+    printf("\nwrite a short-integer number in base-10: ");
+    scanf("%hu", &a);
+    printf("The entered short-integer number in base-10 is: %hu \n", a);
+
+    printf("\nwrite a short-integer number in base-8: ");
+    scanf("%ho", &b);
+    printf("The short-integer entered number in base-8 is: %ho \n", b);
+
+    printf("\nwrite a short-integer number in base-16: ");
+    scanf("%hx", &c);
+    printf("The entered short-integer number in base-16 is: %hx \n", c);
 
     return 0;
 }
