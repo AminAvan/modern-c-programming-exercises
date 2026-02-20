3aa9892c76d3c3a03dee4d0076bd7f2600d9751b
Amin Avan <aminavan.hardware@yahoo.com>
2026-02-20 00:31:43 -0500

    UPDATE: exercise\Chapter007\read_write_diff_integers\read_write_diff_integers.c

--- exercise/Chapter007/read_write_diff_integers/read_write_diff_integers.c
+++ exercise/Chapter007/read_write_diff_integers/read_write_diff_integers.c
@@ -3,6 +3,7 @@
 int main(void){
     unsigned int a, b, c;
     short int dd, ee, ff;
+    long int ggg, hhh, mmm;
 
     printf("\nwrite a number in base-10: ");
     scanf("%u", &a);
@@ -18,16 +19,29 @@ int main(void){
 
 
     printf("\nwrite a short-integer number in base-10: ");
-    scanf("%hu", &a);
-    printf("The entered short-integer number in base-10 is: %hu \n", a);
+    scanf("%hu", &dd);
+    printf("The entered short-integer number in base-10 is: %hu \n", dd);
 
     printf("\nwrite a short-integer number in base-8: ");
-    scanf("%ho", &b);
-    printf("The short-integer entered number in base-8 is: %ho \n", b);
+    scanf("%ho", &ee);
+    printf("The short-integer entered number in base-8 is: %ho \n", ee);
 
     printf("\nwrite a short-integer number in base-16: ");
-    scanf("%hx", &c);
-    printf("The entered short-integer number in base-16 is: %hx \n", c);
+    scanf("%hx", &ff);
+    printf("The entered short-integer number in base-16 is: %hx \n", ff);
+
+    
+    printf("\nwrite a long-integer number in base-10: ");
+    scanf("%lu", &ggg);
+    printf("The entered short-integer number in base-10 is: %lu \n", ggg);
+
+    printf("\nwrite a short-integer number in base-8: ");
+    scanf("%lo", &hhh);
+    printf("The short-integer entered number in base-8 is: %lo \n", hhh);
+
+    printf("\nwrite a short-integer number in base-16: ");
+    scanf("%lx", &mmm);
+    printf("The entered short-integer number in base-16 is: %lx \n", mmm);
 
     return 0;
 }
