7de27a92e2cab524b6a1d0487174410a53988706
Amin Avan <aminavan.hardware@yahoo.com>
2026-02-20 09:42:10 -0500

    ADD: exercise\Chapter007\intro_char\intro_char.c

---
+++ exercise/Chapter007/intro_char/intro_char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    char ch_1, ch_2, ch_3, ch_4;
+
+    ch_1 = 'a';
+    ch_2 = 'A';
+    ch_3 = '0';
+    ch_4 = ' ';
+
+    printf("\nch_1: %c\n", ch_1);
+    printf("ch_2: %c\n", ch_2);
+    printf("ch_3: %c\n", ch_3);
+    printf("ch_4: %c\n", ch_4);
+
+}
