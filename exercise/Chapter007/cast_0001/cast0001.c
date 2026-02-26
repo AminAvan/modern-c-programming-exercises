149f0379f2242a3097136e67960479460a4563ec
Amin Avan <aminavan.hardware@yahoo.com>
2026-02-26 00:00:00 -0500

    ADD: exercise\Chapter007\cast_0001\cast0001.c

---
+++ exercise/Chapter007/cast_0001/cast0001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    float quotient ;
+    int dividend = 4, divisor = 3;
+
+    quotient = dividend / divisor ;
+
+    printf("quotient: %f", quotient);
+
+    return 0;
+}
